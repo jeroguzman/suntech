22d09d963833234ade23a9d5d2049d0da0aeab91
jorbeen <jeroguzman@gmail.com>
2021-11-22 17:10:10 -0700

    Primer commit

---
+++ src/components/navigation.jsx
@@ -0,0 +1,72 @@
+import Logo from './logo.png';
+
+export const Navigation = (props) => {
+  return (
+    <nav id='menu' className='navbar navbar-default navbar-fixed-top'>
+      <div className='container'>
+        <div className='navbar-header'>
+          <button
+            type='button'
+            className='navbar-toggle collapsed'
+            data-toggle='collapse'
+            data-target='#bs-example-navbar-collapse-1'
+          >
+            {' '}
+            <span className='sr-only'>Toggle navigation</span>{' '}
+            <span className='icon-bar'></span>{' '}
+            <span className='icon-bar'></span>{' '}
+            <span className='icon-bar'></span>{' '}
+          </button>
+          <div className='logo'>
+            <a className='navbar-brand page-scroll' href='#page-top'>
+            <h2>SunTech Electronics</h2>
+            </a>{' '}
+          </div>  
+        </div>
+
+        <div
+          className='collapse navbar-collapse'
+          id='bs-example-navbar-collapse-1'
+        >
+          <ul className='nav navbar-nav navbar-right'>
+            <li>
+              <a href='#ofertas' className='page-scroll'>
+                Ofertas
+              </a>
+            </li>
+            <li>
+              <a href='#about' className='page-scroll'>
+                Quienes somos
+              </a>
+            </li>
+            <li>
+              <a href='#services' className='page-scroll'>
+                Servicios
+              </a>
+            </li>
+            <li>
+              <a href='#portfolio' className='page-scroll'>
+                Portafolio
+              </a>
+            </li>
+            <li>
+              <a href='#testimonials' className='page-scroll'>
+                Clientes
+              </a>
+            </li>
+            <li>
+              <a href='#team' className='page-scroll'>
+                Equipo
+              </a>
+            </li>
+            <li>
+              <a href='#contact' className='page-scroll'>
+                Contacto
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
---
+++ src/components/ofertas.jsx
@@ -0,0 +1,23 @@
+
+
+export const Ofertas = (props) => {
+
+  
+
+  return (
+    <div id='ofertas' className='text-center'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>OFERTAS DEL MES</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
+            dapibus leonec.
+          </p>
+        </div>
+        <div className='row'>
+        
+        </div>
+      </div>
+    </div>
+  )
+}
